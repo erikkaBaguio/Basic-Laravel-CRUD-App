251c7b948d3ac2175b90a7321338420f48837ce5
Oskie <oskie@mesasix.com>
2016-06-11 06:40:11 +0800

    Basic Templating with Blade

--- app/app/Http/routes.php
+++ app/app/Http/routes.php
@@ -11,6 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', [
+    'as' => 'home',
+    'uses' => 'PagesController@home'
+]);
+
+
+Route::resource('tasks', 'TasksController');
